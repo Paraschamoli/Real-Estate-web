3434726620e52fa9fc8dac22479c2a2c23e32d80
paras chamoli <paraschamoli2592002@gmail.com>
2024-09-16 21:56:07 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import ProfilePage from "./routes/profilePage/profilePage";
 import Login from "./routes/login/login";
 import Register from "./routes/register/register";
 import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage";
-import NewPostPage from "./routes/newPostPage/NewPostPage";
+import NewPostPage from "./routes/newPostPage/newPostPage";
 
 import {
   listPageLoader,
